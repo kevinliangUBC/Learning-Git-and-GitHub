9c3241008e0cafeb6235e75b95e4805384564e5f
Kevin Liang <kevinjiahongliang@gmail.com>
2017-12-30 18:49:19 -0800

    Added car.java file

---
+++ car.java
@@ -0,0 +1,15 @@
+public class car {
+	private int speed;
+
+	public car() {
+		speed = 0;
+	}
+
+	public void changeSpeed(int i) {
+		speed = i;
+	}
+
+	public int getSpeed() {
+		return speed;
+	}
+}
